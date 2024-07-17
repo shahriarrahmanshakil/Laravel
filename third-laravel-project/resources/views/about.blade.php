2e24b68ee8a50bb12a15cb1cf7fa18585d5c9f5f
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-17 21:20:10 +0600

    third-laravel-project added

---
+++ third-laravel-project/resources/views/about.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+        echo "<h1>About</h1>";
+        echo "A home, or domicile, is a space used as a permanent or semi-permanent residence for one or more human occupants, and sometimes various companion animals. It is a fully- or semi-sheltered space and can have both interior and exterior aspects to it.[vague] Homes provide sheltered spaces, for instance rooms, where domestic activity can be performed such as sleeping, preparing food, eating and hygiene as well as providing spaces for work and leisure such as remote working, studying and playing.";
+
+    ?>
+    
+    <br><br>
+
+    <button>
+        <a href="/">Back</a>
+    </button>
+    
+</body>
+</html>
---
+++ third-laravel-project/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
