7f441f78ac83155672aecfd554200f42712a48a6
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-07-24 20:32:04 +0600

    fourth laravel project added

--- fourth-laravel-project/routes/web.php
+++ fourth-laravel-project/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home/{id}', function (string $id) {
+    return "<h1>Home ID : $id</h1>";
+});
