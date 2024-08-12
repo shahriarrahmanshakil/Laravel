252ade0cce8aaabd397b4e857012d94eb12df51b
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-08-12 15:51:28 +0600

    routing parameter and constrain added

--- laravel-project-01/routes/web.php
+++ laravel-project-01/routes/web.php
@@ -6,15 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/post/{id?}/comment/{cmt_id?}', function (string $id = null, string $cmt_id = null) {
+Route::get('/post/{id}', function (string $id) {
     // return view('post');
     if($id){
-        return "<h1>Post ID : $id</h1>
-                <h2>Comment ID : $cmt_id</h2>";
+        return "<h1>Post ID : $id</h1>";
     }else{
-        return "<h1>No ID Found</h1>";
+        return "<h1>No ID Found</h1>";               
     }
-});
+})->whereIn('id',['Bangladesh','India','Nepal']);                                                                               
 
 Route::get('/sub', function () {
     return view('delete');
