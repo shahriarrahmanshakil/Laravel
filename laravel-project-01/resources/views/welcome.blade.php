0b3b4c1fc76a945a14290932802c7e48a3a7da0c
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-12 09:56:13 +0600

    laravel-project-01 added

---
+++ laravel-project-01/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel-project-01</title>
+    </head>
+    <body>
+        <h1>Laravel Project-01</h1>
+    </body>
+</html>
