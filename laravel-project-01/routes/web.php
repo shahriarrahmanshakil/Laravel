11cf7410dad3a85d3229103fc09ed53f228664f5
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-08-12 15:20:36 +0600

    routing updated

--- laravel-project-01/routes/web.php
+++ laravel-project-01/routes/web.php
@@ -6,10 +6,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/post/{id?}', function (string $id = null) {
+Route::get('/post/{id?}/comment/{cmt_id?}', function (string $id = null, string $cmt_id = null) {
     // return view('post');
     if($id){
-        return "<h1>Post ID : $id</h1>";
+        return "<h1>Post ID : $id</h1>
+                <h2>Comment ID : $cmt_id</h2>";
     }else{
         return "<h1>No ID Found</h1>";
     }
