27e49871a69ce6aae073709a86cc6eaaf0a99ab7
Shahriar Rahman Shakil <shahriarrahmanshakil@gmail.com>
2024-08-17 19:07:41 +0600

    laravel-project-added

---
+++ tour-travel-management/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Frontend\IndexController;
+use App\Http\Controllers\Frontend\AboutController;
+use App\Http\Controllers\Frontend\BookingController;
+use App\Http\Controllers\Frontend\ContactController;
+use App\Http\Controllers\Frontend\DestinationController;
+use App\Http\Controllers\Frontend\PackageController;
+use App\Http\Controllers\Frontend\ServiceController;
+use App\Http\Controllers\Frontend\TeamController;
+use App\Http\Controllers\Frontend\TestimonialController;
+
+Route::get('/',[IndexController::class,'index']);
+Route::get('/about',[AboutController::class,'about']);
+
