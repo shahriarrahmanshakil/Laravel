9c2fc2fcef9ecfc59bb0e7e956351796e3f3bf0d
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-28 12:45:40 +0600

    table reinitialize

--- laravel-database-migration/database/migrations/2024_08_27_191614_create_students_table.php
+++ laravel-database-migration/database/migrations/2024_08_27_191614_create_students_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->string('details');
+            $table->string('email')->nullable();
             $table->timestamps();
         });
     }
