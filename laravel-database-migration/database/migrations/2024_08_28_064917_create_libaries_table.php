b32ff1baff97a7f6fd52265a29d8b5777c0274be
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-08-28 12:50:10 +0600

    new migration added

--- laravel-database-migration/database/migrations/2024_08_28_064917_create_libaries_table.php
+++ laravel-database-migration/database/migrations/2024_08_28_064917_create_libaries_table.php
@@ -11,10 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('libaries', function (Blueprint $table) {
             $table->id();
-            $table->string('name')->unique();
-            $table->string('email')->nullable()->unique();
             $table->timestamps();
         });
     }
@@ -24,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('libaries');
     }
 };
