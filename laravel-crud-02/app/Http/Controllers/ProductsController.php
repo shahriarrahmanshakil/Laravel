b06717220b302721f699897ed1f039e91a2a43fe
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-10-24 08:36:31 +0600

    created table and controller

---
+++ laravel-crud-02/app/Http/Controllers/ProductsController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    //This method will show products page
+    public function index() {
+
+    }
+
+    //This method will show create product page 
+    public function create() {
+
+    }
+
+    //This method will store a product in Database
+    public function store() {
+
+    }
+
+    //This method will show edit product page 
+    public function edit() {
+        
+    }
+}
