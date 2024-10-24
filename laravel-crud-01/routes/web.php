05d8ef7a40d4db7fdde7e19c9d58cf153b18ea86
shahriarrahmanshakil <shahriarrahmanshakil@gmail.com>
2024-10-24 07:10:46 +0600

    updated

--- laravel-crud-01/routes/web.php
+++ laravel-crud-01/routes/web.php
@@ -7,12 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/product', [ProductController::class, 'index'])->name('products.index');
-Route::get('/product/create', [ProductController::class, 'create'])->name('products.name');
-Route::post('/product', [ProductController::class, 'store'])->name('products.store');
-Route::get('/product/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::put('/product/{product}/update', [ProductController::class, 'update'])->name('products.update');
-Route::delete('/product/{product}/destroy', [ProductController::class, 'destroy'])->name('products.destroy');
+Route::get    ( '/product',                   [ProductController::class, 'index'  ]) ->name( 'products.index'  );
+Route::get    ( '/product/create',            [ProductController::class, 'create' ]) ->name( 'products.name'   );
+Route::post   ( '/product',                   [ProductController::class, 'store'  ]) ->name( 'products.store'  );
+Route::get    ( '/product/{product}/edit',    [ProductController::class, 'edit'   ]) ->name( 'products.edit'   );
+Route::put    ( '/product/{product}/update',  [ProductController::class, 'update' ]) ->name( 'products.update' );
+Route::delete ( '/product/{product}/destroy', [ProductController::class, 'destroy']) ->name( 'products.destroy');
 
 
 
